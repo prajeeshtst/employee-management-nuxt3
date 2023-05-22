d21fecc39f58dac5fdd792ef86d181f826a0f728
Prajeesh P <prajeesh+3@techgentsia.com>
2023-05-22 16:23:10 +0530

    fix: style update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   ssr: false,
   modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt','nuxt-icon'],
+  css: [
+    // CSS file in the project
+    '~/assets/css/main.css',
+  ],
   pinia: {
     autoImports: ['defineStore', 'acceptHMRUpdate'],
   },
