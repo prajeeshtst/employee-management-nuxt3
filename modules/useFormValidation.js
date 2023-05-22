2ffd88a4daf4fa1eba4b47fe98f27d0785867151
Prajeesh P <prajeesh+3@techgentsia.com>
2023-05-22 15:13:23 +0530

    fix: validation fixes

--- modules/useFormValidation.js
+++ modules/useFormValidation.js
@@ -1,16 +1,15 @@
-import { ref } from 'vue'
-const errors = ref({});
+ const errors = {}
 import useValidators from './validators'
 export default function useFormValidation() {
     const { isEmpty, minLength, isEmail } = useValidators();
     const validateNameField = (fieldName, fieldValue) => {
-        errors[fieldName] = !fieldValue ? isEmpty(fieldName, fieldValue) : minLength(fieldName, fieldValue, 4)
+        errors[fieldName] = isEmpty(fieldName, fieldValue)
     }
+    const validateSelectField = (fieldName, fieldValue) => {
+      errors[fieldName] = isEmpty(fieldName, fieldValue)
+  }
     const validateEmailField = (fieldName, fieldValue) => {
         errors[fieldName] = !fieldValue ? isEmpty(fieldName, fieldValue) : isEmail(fieldName, fieldValue)
     }
-    const validateSelectField = (fieldName, fieldValue) => {
-        errors[fieldName] = isEmpty(fieldName, fieldValue)
-    }
-    return { errors, validateNameField, validateEmailField,validateSelectField}
+    return { errors, validateNameField, validateEmailField,validateSelectField }
 }
